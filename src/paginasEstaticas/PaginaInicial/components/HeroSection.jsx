3892ed05dac1c3b139f21d7fae7f9a25eeb1aecb
LuanSilva9 <luanpdev@gmail.com>
2024-11-01 23:48:02 -0400

    Fazendo responsividade da pagina inicial

--- src/paginasEstaticas/PaginaInicial/components/HeroSection.jsx
+++ src/paginasEstaticas/PaginaInicial/components/HeroSection.jsx
@@ -1,7 +1,17 @@
 export default function HeroSection() {
     return (
         <section className="heroSection">
-            <div className="image"></div>
+            <div className="image">
+                <div className="content">
+                    <h2 className="title">Oi, eu me chamo Luan e esse é meu portifolio...</h2>
+                    
+                    <div className="tags">
+                        <p>Desenvolvedor de Software</p>
+                        <p>Analista de Sistemas</p>
+                        <p>Analista de Dados</p>        
+                    </div>
+                </div>
+            </div>
             <div className="text">
                 <div className="content">
                     <h2 className="title">Oi, eu me chamo Luan e esse é meu portifolio...</h2>
